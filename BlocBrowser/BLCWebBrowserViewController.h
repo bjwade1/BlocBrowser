1c7a7b72dc011ac02631d5dcc5416339ff4b7981
Brandon Wade <brandon.wade3@gmail.com>
2015-07-28 15:57:43 -0500

    Functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Brandon Wade on 7/28/15.
+//  Copyright (c) 2015 Brandon Wade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
